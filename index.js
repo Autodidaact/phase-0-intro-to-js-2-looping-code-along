8c0bb1af8a872afc88041e5143adfbf3b6694d13
Ibrahim Abdulai Yakubu <ibrahimay295@gmail.com>
2022-06-12 21:09:45 +0000

    js 2 looping code along lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function writeCards(namesArray = ["Guadalupe", "Ollie", "Aki"], keyWord = "suprise"){
+    let messageArray = [];
+    for (let i = 0; i < namesArray.length; i++) {
+       
+        messageArray.push(`Thank you, ${namesArray[i]}, for the wonderful ${keyWord} gift!`);
+        
+        
+         
+       
+    
+    }
+    return messageArray;
+}
+console.log(writeCards());
+
+function countDown(num){
+    let counter = 0
+    for (counter; counter <= num; num--){
+        console.log(num);
+    }
+}
